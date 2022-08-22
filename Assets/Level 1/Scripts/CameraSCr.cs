27fee0df62e27c778f07a1b511c1bdb3f8968a46
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-08-22 15:57:16 +0300

    Commit 3

--- Assets/Level 1/Scripts/CameraSCr.cs
+++ Assets/Level 1/Scripts/CameraSCr.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class CameraSCr : MonoBehaviour
 {
     private float yRotation;
-
+    [SerializeField] private Transform playerTransform;
+    private Quaternion lookRotation;
+    [SerializeField] private Transform cameraTransform;
     void Update()
     {
         RotatePivot();
+    }
+
+    public void LateUpdate()
+    {
+        
+
+        Vector3 lookDirection = lookRotation *  new Vector3(0, -0.5f ,1);
+        Vector3 lookPosition = playerTransform.position - lookDirection * 3.6f;
+
+        if (Physics.Raycast(playerTransform.position, -lookDirection, out RaycastHit hit, 3.6f))
+        {
+            lookPosition = playerTransform.position - lookDirection * hit.distance;
+        }
+        cameraTransform.SetPositionAndRotation(lookPosition, cameraTransform.rotation);
+        
     }
 
     private void RotatePivot()
@@ -17,7 +34,8 @@ public class CameraSCr : MonoBehaviour
         {
             float yPivot = Input.GetAxis("Mouse X") * 600 * Time.fixedDeltaTime;
             yRotation += yPivot;
-            gameObject.transform.localEulerAngles = new Vector3(0f, yRotation, 0f);
-        }        
+            lookRotation = Quaternion.Euler(0, yRotation, 0);
+            transform.SetPositionAndRotation(Vector3.one, lookRotation);
+        }
     }
 }
