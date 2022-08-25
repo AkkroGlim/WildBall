59cca2a73218411edfc0e5ce0fe8be31415363ac
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-08-25 20:41:43 +0300

    Commit 4

--- Assets/MainMenu/Scripts/SettingsScript.cs
+++ Assets/MainMenu/Scripts/SettingsScript.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
-    private bool isFullScreen = true;
-
-    Resolution[] rsl;
-    List<string> resolutions;
+    private bool fullScreenFlag;
+    private static Resolution[] rsl;
+    private static List<string> resolutions;
     [SerializeField] private Dropdown dropdown;
+    [SerializeField] private Toggle isFullScreenTogle;
     
-
 
 
     public void Awake()
     {
+        
         resolutions = new List<string>();
         rsl = Screen.resolutions;
         foreach (var i in rsl)
@@ -25,12 +25,21 @@ public class SettingsScript : MonoBehaviour
         dropdown.ClearOptions();
         dropdown.AddOptions(resolutions);
         dropdown.value = 12;
+        if (!isFullScreenTogle.isOn.Equals(Screen.fullScreen))
+        {
+            isFullScreenTogle.isOn = Screen.fullScreen;
+            
+        }
+        fullScreenFlag = true;
+
+
+
     }
 
-
+    
     public void Resolution(int r)
     {
-        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+        //Screen.SetResolution(rsl[r].width, rsl[r].height, Screen.fullScreen);
     }
 
     public void Quality(int q)
@@ -41,8 +50,10 @@ public class SettingsScript : MonoBehaviour
 
     public void FullScreenToggle()
     {
-       
-        isFullScreen = !isFullScreen;
-        Screen.fullScreen = isFullScreen;
+        if (fullScreenFlag)
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+               
     }
 }
