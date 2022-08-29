742b193d3c9650d79e0ce4823b837b8137ee1eb5
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-08-29 03:01:55 +0300

    4.2

--- Assets/MainMenu/Scripts/VolumeScript.cs
+++ Assets/MainMenu/Scripts/VolumeScript.cs
@@ -8,11 +8,30 @@ public class VolumeScript : MonoBehaviour
 {
     [SerializeField] private AudioMixer am;
     [SerializeField] private Slider slid;
+    private static float[] slidersValue = new float[3];
+    private string[] slidersName = new string[3] {"masterVolume", "musicVolume", "soundsVolume" };
 
     private void OnEnable()
     {
-       
-        
+        for( int i = 0; i < slidersName.Length; i++)
+        {
+            if (gameObject.name.Equals(slidersName[i]))
+            {
+                slid.value = slidersValue[i];
+            }
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < slidersName.Length; i++)
+        {
+            if (gameObject.name.Equals(slidersName[i]))
+            {
+                slidersValue[i] = slid.value;
+            }
+        }
     }
     public void AudioVolume(float sliderValue)
     {
