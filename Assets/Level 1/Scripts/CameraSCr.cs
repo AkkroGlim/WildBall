3c87ffd928f5f056a6d7fda88543a163ad5ffb6a
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-09-07 04:02:02 +0300

    Commit 6.1

--- Assets/Level 1/Scripts/CameraSCr.cs
+++ Assets/Level 1/Scripts/CameraSCr.cs
@@ -5,34 +5,44 @@ using UnityEngine;
 public class CameraSCr : MonoBehaviour
 {
     private float yRotation;
+    private float xRotation;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform pointPivot;
     private Quaternion lookRotation;
+    private Quaternion pointPivotRotation;
     [SerializeField] private Transform cameraTransform;
     void Update()
-    {
-        RotatePivot();
+    {        
+        RotatePivot();        
     }
 
     public void LateUpdate()
     {
-        Vector3 lookDirection = lookRotation *  new Vector3(0, -0.5f ,1);
+        Vector3 lookDirection = lookRotation * new Vector3(0, -0.5f, 1);
         Vector3 lookPosition = playerTransform.position - lookDirection * 3.6f;
 
         if (Physics.Raycast(playerTransform.position, -lookDirection, out RaycastHit hit, 3.6f))
         {
             lookPosition = playerTransform.position - lookDirection * hit.distance;
         }
-        cameraTransform.SetPositionAndRotation(lookPosition, cameraTransform.rotation);       
+        cameraTransform.SetPositionAndRotation(lookPosition, cameraTransform.rotation);
     }
 
     private void RotatePivot()
     {
-        if(Time.timeScale > 0)
+        if (Time.timeScale > 0)
         {
             float yPivot = Input.GetAxis("Mouse X") * 600 * Time.fixedDeltaTime;
+            float xPivot = Input.GetAxis("Mouse Y") * 600 * Time.fixedDeltaTime;
             yRotation += yPivot;
-            lookRotation = Quaternion.Euler(0, yRotation, 0);
+            xRotation -= xPivot;
+            xRotation = Mathf.Clamp(xRotation, -30f, 30f);
+            pointPivotRotation = Quaternion.Euler(0, yRotation, 0);
+            lookRotation = Quaternion.Euler(xRotation, yRotation, 0);
+            pointPivot.SetPositionAndRotation(Vector3.one, pointPivotRotation);
             transform.SetPositionAndRotation(Vector3.one, lookRotation);
         }
     }
+
+    
 }
