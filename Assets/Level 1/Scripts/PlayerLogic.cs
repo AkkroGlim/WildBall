7cd1f01e507140136b1676092df2ed7d9214438c
AkkroGlim <111155608+AkkroGlim@users.noreply.github.com>
2022-09-15 20:38:43 +0300

    Commit 7

--- Assets/Level 1/Scripts/PlayerLogic.cs
+++ Assets/Level 1/Scripts/PlayerLogic.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+
+
+
 
 public class PlayerLogic : MonoBehaviour
 {
@@ -13,19 +17,26 @@ public class PlayerLogic : MonoBehaviour
     [SerializeField] private Transform frontPoint;
     [SerializeField] private Transform sidePoint;
     [SerializeField] private GameObject player;
-
-
+    private Vector3 groundPoint = Vector3.down;
+    
     private bool isJump;
     
     private bool isGrounded;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (true)
-        {
-            //collision.collider.ClosestPoint(transform.position)
+        if (playerCol.ClosestPoint(collision.contacts[collision.contacts.Length - 1].point).y < transform.position.y - playerCol.bounds.extents.y / 2)
+        {                        
+            groundPoint = playerCol.ClosestPoint(collision.contacts[collision.contacts.Length - 1].point) - transform.position;           
+            Debug.Log(groundPoint);
         }
-        
+        else
+        {
+            groundPoint = Vector3.down;
+        }
+
+
+
     }
     public void Start()
     {
@@ -75,11 +86,13 @@ public class PlayerLogic : MonoBehaviour
     }
 
     private void jump()
-    {      
+    {
+        
         if (isJump && isGrounded)
         {
-            isJump = false;           
-            playerBody.AddForce(new Vector3(0f, jumpForce, 0f) * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);           
+            isJump = false;
+            playerBody.AddForce(new Vector3(0f, jumpForce, 0f) * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+            
         }
     }
 
@@ -87,7 +100,7 @@ public class PlayerLogic : MonoBehaviour
     {
         //isGrounded = Physics.Raycast(gameObject.transform.position, Vector3.down, playerCol.bounds.extents.y + 0.01f);
         RaycastHit hit;
-        isGrounded = Physics.Raycast(gameObject.transform.position, Vector3.down, out hit, playerCol.bounds.extents.y + 0.01f);
+        isGrounded = Physics.Raycast(gameObject.transform.position, groundPoint, out hit, playerCol.bounds.extents.y + 0.01f);
         if (isGrounded)
         {
             player.transform.SetParent(hit.transform);
